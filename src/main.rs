41d67f96d4c5572151ea8a807245ccaa869e5e14
mxdblcf <mxdblcf@163.com>
2023-07-31 23:35:23 +0800

    add rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
+
+    //打印字符
     println!("Hello, world!");
+    let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Error reading line");
+println!("you guessed {guess}");
 }
