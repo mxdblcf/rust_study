63ea8a0fe832e97169312ae0abc77850bb64c54a
Alex Ma <mxdblcf@163.com>
2023-08-07 11:01:06 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use std::io;
+
 
 fn main() {
 
     //打印字符
     println!("Hello, world!");
-    let mut guess = String::new();
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Error reading line");
-println!("you guessed {guess}");
+
 }
