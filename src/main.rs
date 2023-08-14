2fd1a095dee375057ae344373a2c895cb89bdb19
mxdblcf <mxdblcf@163.com>
2023-08-14 15:46:43 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 
+
 fn main() {
 
     //打印字符
     println!("Hello, world!");
-<<<<<<< HEAD
-println!("mxd");
-}
-=======
 
-    println!("Hello, world!mxd");
+ println!("mx1jjjjd");
+ // set variable
+ let x = 5 ;   //is immutable variable
+ println!("The variable value is ={x}");
+//  x=6;   cannot assign twice to immutable variable
+//  println!("The variable value is ={x}");
+ 
+ let mut x1 = 5 ; // add "mut" and the variable is changeable ,use mut mark ,the variable must be same type
+ println!("The variable value is ={x1}");
+ let  x1 = 51 ; // 
+ println!("The variable value is ={x1}");
+ //data type
+ let stringtToNum:u32= "1".parse().expect("error 1111");
+ println!("{stringtToNum}");
 }
->>>>>>> d7917967622000934928df7727f5f03e298c61ff
